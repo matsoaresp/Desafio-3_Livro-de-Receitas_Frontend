83c6dee267d64303bbfbb918349763f43269b8cd
Mateus Prado <maateussp@gmail.com>
2026-04-20 15:54:50 -0300

    feat: Styling the code with Tailwind.

--- src/components/layout/Section.tsx
+++ src/components/layout/Section.tsx
@@ -1,13 +1,21 @@
-import { Container } from "./Container";
 import { Form } from "./Form";
 
-export function Section (){
-    return (
-       <>
-        <h1>Categorias e pratos</h1>
-        <Form>
-            
-        </Form>
-       </>
-    )
+export function Section() {
+  return (
+    <section className="bg-white w-full max-w-[1175px] flex flex-col items-center rounded-lg mx-auto min-h-screen px-4 overflow-hidden">
+
+      <div className="flex flex-col items-center mt-20 w-full">
+        <h1 className="text-[26px] font-medium text-center">
+          Categorias e pratos
+        </h1>
+
+        <Form />
+      </div>
+
+      <h2 className="mt-10 font-medium text-[22px]">
+        Categorias
+      </h2>
+
+    </section>
+  );
 }
